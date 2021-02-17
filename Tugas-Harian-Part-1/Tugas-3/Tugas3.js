fa7b3069d5d65ab622e671563ba397cc1b5c58dd
LevraGav <arvel.gavrilla@gmail.com>
2021-02-17 12:49:36 +0700

    jawaban soal no 1

---
+++ Tugas-Harian-Part-1/Tugas-3/Tugas3.js
@@ -0,0 +1,16 @@
+//soal 1
+var kata1 = "saya";
+var kata2 = "senang";
+var kata3 = "belajar";
+var kata4 = "javascript";
+
+var kata2_upper_s = kata2[0].toUpperCase(); //mengubah S menjadi kapital
+var kata4_upper = kata4.toUpperCase(); //mengubah kataKeempat jadi kapital semua
+
+console.log(kata1.concat(" " + kata2_upper_s + kata2.slice(1), " " + kata3, " " + kata4_upper));
+// saya Senang belajar JAVASCRIPT
+
+
+
+
+
