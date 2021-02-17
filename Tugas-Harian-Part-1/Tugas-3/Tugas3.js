8f65218faf11feaac15222aee42d81645b01dc8b
Arvel Gavrilla R <arvel.gavrilla@gmail.com>
2021-02-17 12:51:15 +0700

    penyelesaian soal no 2

--- Tugas-Harian-Part-1/Tugas-3/Tugas3.js
+++ Tugas-Harian-Part-1/Tugas-3/Tugas3.js
@@ -10,6 +10,19 @@ var kata4_upper = kata4.toUpperCase(); //mengubah kataKeempat jadi kapital semua
 console.log(kata1.concat(" " + kata2_upper_s + kata2.slice(1), " " + kata3, " " + kata4_upper));
 // saya Senang belajar JAVASCRIPT
 
+//soal 2
+var string1 = "1";
+var string2 = "2";
+var string3 = "4";
+var string4 = "5";
+
+var number1 = parseInt(string1); // 1
+var number2 = parseInt(string2); // 2
+var number3 = parseInt(string3); // 4
+var number4 = parseInt(string4); // 5
+
+console.log(number1 + number2 + number3 + number4); //12
+
 
 
 
