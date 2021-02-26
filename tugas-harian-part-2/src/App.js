31539d62659c4805488d11737ee6969aefe66188
Arvel Gavrilla R <arvel.gavrilla@gmail.com>
2021-02-26 22:45:15 +0700

    Update App.js

--- tugas-harian-part-2/src/App.js
+++ tugas-harian-part-2/src/App.js
@@ -1,63 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import FormBuah from './Tugas-9/FormBuah'
+import Tampilkan from './Tugas-10/ListBuah'
 
 function App() {
-  return (
-    <div className="content">
-      <h1>Form Pembelian Buah</h1>
-      <table>
-        <tr>
-          <td>
-            <label for="nama"><b>Nama Pelanggan</b></label> 
-          </td>
-          <div className="box">
-            <td>
-              <input type="text" id="nama" size="21"></input>
-            </td>
-          </div>
-        </tr>
-        <tr>
-          <td>
-            <label for="daftar-item"><b>Daftar Item</b></label> 
-          </td>
-          <td className="iteminput">
-            <div className="box">
-              <input type="checkbox" name="daftar-item" value="Semangka"></input>
-              <label className="fruit">Semangka</label>
-              <br></br>
-            </div>
-
-            <div className="box">
-            <input type="checkbox" name="daftar-item" value="Semangka"></input>
-            <label className="fruit">Jeruk</label>
-            <br></br>
-            </div>
-
-            <div className="box">
-            <input type="checkbox" name="daftar-item" value="Semangka"></input>
-            <label className="fruit">Nanas</label>
-            <br></br>
-            </div>
-
-            <div className="box">
-            <input type="checkbox" name="daftar-item" value="Semangka"></input>
-            <label className="fruit">Salak</label>
-            <br></br>
-            </div>
-
-            <div className="box">
-            <input type="checkbox" name="daftar-item" value="Semangka"></input>
-            <label className="fruit">Anggur</label>
-            <br></br>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button>Kirim</button>
-          </td>
-        </tr>
-      </table>
+  return(
+    <div>
+      <FormBuah />
+      <Tampilkan />
     </div>
   );
 }
